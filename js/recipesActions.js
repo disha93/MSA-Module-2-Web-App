6a496774d7cc317b8af272ae48d5dd7bf97b05f6
Shomal Dass <Shomal Dass>
2016-09-15 16:05:44 +1200

    Add project files and folders

---
+++ js/recipesActions.js
@@ -0,0 +1,33 @@
+
+function retrieveDisplayRecipes() {
+	var searchText = document.getElementById('searchBox').value;
+}
+
+function loadTopRatedRecipes() {
+	$.ajax({
+		url: 'http://food2fork.com/api/search?',
+		dataType: 'json',
+		headers: {
+			'Content-Type': 'application/json',
+			'Ocp-Apim-Subscription-Key': 'bf04268391ac462acdd61749bc647c6b'},
+		type: "GET",
+		success: function(data){
+			showRecipes(data);
+		}
+	})
+}
+
+function showRecipes(data){
+	var recipesObj = JSON.parse(data);
+	var tableContent = "";
+	for(var i=0; i<recipesObj.length; i++){
+		var currentRecipe = recipesObj[i];
+		
+		tableContent += '<tr>' + 
+		'<td><img src = "' + currentRecipe.image_url + '"></td>' +
+		'<td>' + currentRecipe.title + '"></td>' +
+		'<td>' + currentRecipe.source_url + '"></td>' + 
+		'</tr>' + '\n';
+	}
+	document.getElementById('recipeResults').innerHTML = tableContent;
+}
